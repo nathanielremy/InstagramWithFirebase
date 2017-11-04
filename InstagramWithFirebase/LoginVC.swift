26aa5ea067c145e92bc522e0b8ed51e61d04d14d
Nathaniel Remy <nathanielremy1@gmail.com>
2017-11-04 20:13:10 +0100

    LoginVC file added

--- InstagramWithFirebase/LoginVC.swift
+++ InstagramWithFirebase/LoginVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginVC.swift
 //  InstagramWithFirebase
 //
 //  Created by Nathaniel Remy on 04/11/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
