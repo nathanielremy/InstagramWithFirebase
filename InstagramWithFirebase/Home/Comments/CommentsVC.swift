a6dd684c0d0d672944608025ba5b4b0afc6c20e4
Nathaniel Remy <nathanielremy1@gmail.com>
2017-11-25 18:28:08 +0100

    CommentsVC inputAccessoryView implemented correctly

--- InstagramWithFirebase/Home/Comments/CommentsVC.swift
+++ InstagramWithFirebase/Home/Comments/CommentsVC.swift
@@ -10,9 +10,66 @@ import UIKit
 
 class CommentsVC: UICollectionViewController {
     
+    // Stored properties
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        
+        let sendButton = UIButton(type: .system)
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.setTitleColor(.black, for: .normal)
+        sendButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        sendButton.addTarget(self, action: #selector(handleSend), for: .touchUpInside)
+        
+        containerView.addSubview(sendButton)
+        sendButton.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: -12, width: 50, height: nil)
+        
+        
+        let commentTextField = UITextField()
+        commentTextField.placeholder = "Enter Comment"
+        
+        containerView.addSubview(commentTextField)
+        commentTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: sendButton.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: nil, height: nil)
+        
+        return containerView
+    }()
+    
+    @objc func handleSend() {
+        print("Handle Send")
+    }
+    
+    // Don't have to handle the position of the view
+    // UIKit will do all of the work for you
+    override var inputAccessoryView: UIView? {
+        get {
+            return self.containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
+        
         collectionView?.backgroundColor = .red
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Hide the tabBar when the viewController will appear
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Reshow the tabBar when the viewController gets dismissed
+        tabBarController?.tabBar.isHidden = false
     }
 }
